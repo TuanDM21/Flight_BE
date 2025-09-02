5db466a9b6d86384669d0ec6196a9033a1be61c0
tinhnguyen0110 <tinh2kqb@gmail.com>
2025-09-02 10:30:29 +0700

    remove backup file

--- backup_responses_20250831_155759/response/auth/ApiLoginResponse.java
+++
@@ -1,9 +0,0 @@
-package com.project.quanlycanghangkhong.dto.response.auth;
-
-import com.project.quanlycanghangkhong.dto.response.ApiResponseCustom;
-
-import io.swagger.v3.oas.annotations.media.Schema;
-
-@Schema(description = "API response for login, data is LoginResponse")
-public class ApiLoginResponse extends ApiResponseCustom<LoginResponse> {
-}
--- backup_responses_20250831_155759/response/auth/ApiRegisterResponse.java
+++
@@ -1,9 +0,0 @@
-package com.project.quanlycanghangkhong.dto.response.auth;
-
-import com.project.quanlycanghangkhong.dto.response.ApiResponseCustom;
-
-import io.swagger.v3.oas.annotations.media.Schema;
-
-@Schema(description = "API response for register, data is RegisterResponse")
-public class ApiRegisterResponse extends ApiResponseCustom<RegisterResponse> {
-}
